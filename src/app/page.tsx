c45fda190f7c11397da11788ea04b96a8d0f7376
louisdevzz <vohuunhan1310@gmail.com>
2024-09-14 20:40:53 +0700

    fix: input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+"use client"
 import Siderbar from "@/components/Siderbar"
+import { useState } from "react";
 
 export default function Home() {
+
+  const [isInput, setIsInput] = useState<boolean>(false)
+
   return (
     <div className="flex flex-row gap-5 w-full">
       <Siderbar/>
@@ -18,25 +23,39 @@ export default function Home() {
           
         </div>
         <div className="fixed bottom-10 max-w-6xl w-full">
-            <div className="flex flex-row border border-gray-200 rounded-2xl shadow-sm p-3">
-              <input className="w-full outline-none py-2 px-3 text-wrap" type="text" placeholder="Input your evaluation criterria" />
-              <div className="flex flex-row gap-2">
-                <div className="flex flex-row gap-5 border-r border-gray-200 px-2">
+            <div onClick={()=>setIsInput(true)} className={`flex ${isInput?"flex-col w-full float-start":"flex-row"} border border-gray-200 rounded-2xl shadow-sm p-3 items-center`}>
+              {
+                isInput?(
+                  <textarea className="p-2 w-full outline-none resize-none h-32 border-b border-gray-200" placeholder="Input your evaluation criterria"/>
+                ):(
+                  <p className="w-full text-gray-400">Input your evaluation criterria</p>
+                )
+              }
+              <div className={`flex flex-row gap-2 ${isInput&&"w-full float-start mt-2 justify-between"}`}>
+                <div className={`flex flex-row gap-5 px-2 ${!isInput&&"border-r border-gray-200"}`}>
+                  {
+                    isInput&&(
+                      <div className="flex flex-row gap-2 bg-gray-100 p-2 rounded-lg items-center shadow-sm">
+                        <img width={15} src="/assets/library.svg" alt="icon" />
+                        <span>Library</span>
+                      </div>
+                    )
+                  }
                   <button>
-                    <img width={25} src="/assets/paperclip.svg" alt="icon" />
+                    <img width={isInput?20:25} src="/assets/paperclip.svg" alt="icon" />
                   </button>
                   <button>
-                    <img width={25} src="/assets/microphone.svg" alt="icon" />
+                    <img width={isInput?20:25} src="/assets/microphone.svg" alt="icon" />
                   </button>
                   <button>
-                    <img width={25} src="/assets/photo.svg" alt="icon" />
+                    <img width={isInput?20:25} src="/assets/photo.svg" alt="icon" />
                   </button>
                   <button>
-                    <img width={25} src="/assets/grid.svg" alt="icon" />
+                    <img width={isInput?20:25} src="/assets/grid.svg" alt="icon" />
                   </button>
                 </div>
                 <button className="p-3 rounded-xl bg-[#0969DA]">
-                  <img width={20} src="/assets/telegram.svg" alt="icon" />
+                  <img width={isInput?15:25} src="/assets/telegram.svg" alt="icon" />
                 </button>
               </div>
             </div>
